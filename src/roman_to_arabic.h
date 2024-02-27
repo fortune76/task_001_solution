43ab10e097b8ffc2d3acdebd470f33c5676dc0c0
Maksim <pumpbro@gmail.com>
2024-02-27 14:05:03 +0300

    solution

---
+++ src/roman_to_arabic.h
@@ -0,0 +1,18 @@
+#ifndef ROMAN_TO_ARABIC_H
+#define ROMAN_TO_ARABIC_H
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#define ALPHABET "NIVXLCDM"
+
+int convert_roman(char *str);
+void exception_print();
+void output_digit(int res);
+int data_validation(char *str);
+void convert_digit(int num, char *res);
+int check_result(char *input_str, char *expected_str);
+int convert_small_digits(char *str, int num, char *capacity,
+                         char *next_capacity, int position);
+
+#endif
